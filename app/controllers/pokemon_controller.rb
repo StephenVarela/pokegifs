676fb585b0b245ba4afe73ede6309d0168b7e856
StephenVarela <varelast92@gmail.com>
2018-10-23 13:31:34 -0400

    able to query pokeapi and gifapi

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -4,10 +4,22 @@ class PokemonController < ApplicationController
     pokemon_id = params[:id]
     res = HTTParty.get("http://pokeapi.co/api/v2/pokemon/#{pokemon_id}/")
     body = JSON.parse(res.body)
-    puts body["name"] # should be "pikachu"
 
-    render json: { "message": "ok" }
+    pokemon_name = body["name"] # should be "pikachu"
+    pokemon_types = [];
 
+    body["types"].each do |type|
+      pokemon_types << type["type"]["name"]
+    end
+
+    res = HTTParty.get("https://api.giphy.com/v1/gifs/search?api_key=R94laQQCkFjOS7Z5jcVdQ4MkNIx4bNK4&q=pikachu&rating=g")
+    body = JSON.parse(res.body)
+    binding.pry
+
+
+    render json: { "id": pokemon_id,
+                   "name": pokemon_name,
+                   "types": pokemon_types}
 
   end
 end
